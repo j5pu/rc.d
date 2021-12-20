0af6a78a5bf1fab141ec9ce2d0fb9426e5eb1d8a
j5pu <63794670+j5pu@users.noreply.github.com>
2021-12-20 16:07:35 +0100

    works bats libs

--- lib/profile.sh
+++ lib/profile.sh
@@ -2,12 +2,14 @@
 
 # System profile
 #
+[ ! "${PS1-}" ] || set -eu
 export RC_NAME='rc.d'
 export RC_D="/etc/${RC_NAME}"
 export RC_PREFIX="${RC_PREFIX:-${RC_D}}"
 export RC_GENERATED="${RC_PREFIX}/generated.d"
 export RC_PASSWORD_FILE="${RC_GENERATED}/password.sh"
-
-[ "${RC_PROFILE-0}" = 1 ] || "${RC_PREFIX}/bin/rc --force"
+UNAME="$(command -p uname)"; export UNAME
+eval "$("${RC_PREFIX}/bin/paths")"
+[ "${RC_PROFILE-0}" = 1 ] ||
 [ "${UNAME-}" ] || for generated in "${RC_GENERATED}"/*; do . "${generated}"; done
 export RC_PROFILE=1
